e0e26dfb4bcebf95b5bc6f39010aed3b7fa2c20f
AndreyGOIT <ae.borga@gmail.com>
2025-05-05 14:52:28 +0300

    Render State in the User Interface

--- src/react.js
+++ src/react.js
@@ -1,21 +1,21 @@
 import React from 'react'
 // import PropTypes from 'prop-types';
 
-class StatefulComponent extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
-    // Only change code below this line
     this.state = {
-      firstName: "John"
+      name: 'freeCodeCamp'
     }
-    // Only change code above this line
   }
   render() {
     return (
       <div>
-        <h1>{this.state.firstName}</h1>
+        { /* Change code below this line */ }
+        <h1>{this.state.name}</h1>
+        { /* Change code above this line */ }
       </div>
     );
   }
 };
-  export default StatefulComponent;
+  export default MyComponent;
