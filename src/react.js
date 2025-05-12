b4e59d1ebacafe8b34681479e002eb04f8d0c8b9
AndreyGOIT <ae.borga@gmail.com>
2025-05-12 09:30:45 +0300

    Inline Styles as Object

--- src/react.js
+++ src/react.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+const styles = {
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple"
+}
+// Change code above this line
 class Colorful extends React.Component {
   render() {
+    // Change code below this line
     return (
-      <div style={{color: "red", fontSize: "72px"}}>Big Red</div>
+      <div style={styles}>Style Me!</div>
     );
+    // Change code above this line
   }
 };
 
