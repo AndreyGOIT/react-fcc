1e4d5748bbc0b9179883fb3b7b80700127c1a595
AndreyGOIT <ae.borga@gmail.com>
2025-05-23 14:10:50 +0300

    Use Array.filter() to Dynamically Filter an Array

--- src/react.js
+++ src/react.js
@@ -1,26 +1,49 @@
 import React from 'react'
 
-const frontEndFrameworks = [
-  'React',
-  'Angular',
-  'Ember',
-  'Knockout',
-  'Backbone',
-  'Vue'
-];
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(user => user.online); // Change this line
+    const renderOnline = usersOnline.map(user => {
+      return <li key={user.username}>{user.username}</li>;
+    }); // Change this line
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
+}
 
-function Frameworks() {
-  const renderFrameworks = frontEndFrameworks.map(i => {
-    return <li key={i}>{i}</li>;
-  }); // Change this line
-  return (
-    <div>
-      <h1>Popular Front End JavaScript Frameworks</h1>
-      <ul>
-        {renderFrameworks}
-      </ul>
-    </div>
-  );
-};
-
-  export default Frameworks;
+  export default MyComponent;
